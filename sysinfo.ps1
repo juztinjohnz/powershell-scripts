8b4cbc1e03263dc2cd3ec9278b8ddd6fb40ab252
Justin Johns <juztinjohnz@users.noreply.github.com>
2023-03-13 10:14:57 +1100

    Update sysinfo.ps1

--- sysinfo.ps1
+++ sysinfo.ps1
@@ -1,3 +1,7 @@
+#This script uses Windows Management Instrumentation (WMI) and Common Information Model (CIM) to get system information, and the Windows Forms assembly to create a GUI.
+#To run this script, open Windows PowerShell and navigate to the directory where the script is saved. and type .\sysinfo.ps1 
+
+
 # Import Windows Forms assembly
 Add-Type -AssemblyName System.Windows.Forms
 
